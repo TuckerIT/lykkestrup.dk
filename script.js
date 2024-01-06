f628a3c075938e093563dc4a81af5c986db9dc9f
TuckerIT <47447709+TuckerIT@users.noreply.github.com>
2024-01-06 21:15:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ const imageViewer = document.getElementById('imageViewer');
 let currentIndex = 0;
 
 // Hent en liste over billeder fra mappen "images"
-fetch('images')
+fetch('https://api.github.com/repos/tuckerit/lykkestrup.dk/contents/images')
   .then(response => response.json())
   .then(data => {
-    const images = data.tree.filter(item => item.type === 'blob' && item.path.match(/\.(png|jpg|jpeg|gif)$/)).map(item => item.path);
+    const images = data.filter(item => item.type === 'file' && item.name.match(/\.(png|jpg|jpeg|gif)$/)).map(item => item.download_url);
     
     // Hvis der er billeder, start billedviseren
     if (images.length > 0) {
-      imageViewer.src = `images/${images[currentIndex]}`;
+      imageViewer.src = images[currentIndex];
       
       // Skift billede ved klik
       imageViewer.addEventListener('click', () => {
         currentIndex = (currentIndex + 1) % images.length;
-        imageViewer.src = `images/${images[currentIndex]}`;
+        imageViewer.src = images[currentIndex];
       });
     } else {
       console.error('Ingen billeder blev fundet i mappen "images".');
@@ -24,3 +24,4 @@ fetch('images')
   .catch(error => {
     console.error('Fejl ved indlæsning af billeder:', error);
   });
+

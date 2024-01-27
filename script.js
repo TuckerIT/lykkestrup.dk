7f51140e4ac45817469dcf48301d8031ca85d123
TuckerIT <47447709+TuckerIT@users.noreply.github.com>
2024-01-27 19:59:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,39 +11,44 @@ function changeImage() {
 
 function startAutoChange() {
     intervalId = setInterval(() => {
-    
-            changeImage();
-        
+        changeImage();
     }, 20000);
 }
 
 // Hent en liste over billeder fra mappen "images"
 fetch('https://api.github.com/repos/tuckerit/lykkestrup.dk/contents/images')
-  .then(response => response.json())
-  .then(data => {
-    images = data.filter(item => item.type === 'file' && item.name.match(/\.(png|jpg|jpeg|gif)$/)).map(item => item.download_url);
-    
-    // Hvis der er billeder, start billedviseren
-    if (images.length > 0) {
-      imageViewer.src = images[currentIndex];
-      
-      // Skift automatisk billede efter 10 sekunder
-      startAutoChange();
+    .then(response => response.json())
+    .then(data => {
+        images = data.filter(item => item.type === 'file' && item.name.match(/\.(png|jpg|jpeg|gif)$/)).map(item => item.download_url);
 
-      // Skift billede ved klik (desktop)
-      imageViewer.addEventListener('click', () => {
-    
-            clearInterval(intervalId); // Stop den automatiske skift, når der klikkes
-            changeImage();
-            startAutoChange(); // Start den automatiske skift igen
-        
-      });
+        // Hvis der er billeder, start billedviseren
+        if (images.length > 0) {
+            imageViewer.src = images[currentIndex];
 
-     
-    } else {
-      console.error('Ingen billeder blev fundet i mappen "images".');
-    }
-  })
-  .catch(error => {
-    console.error('Fejl ved indlæsning af billeder:', error);
-  });
+            // Skift automatisk billede efter 10 sekunder
+            startAutoChange();
+
+            // Skift billede ved klik (desktop)
+            if (navigator.userAgent.toLowerCase().includes('mobile') || navigator.userAgent.toLowerCase().includes('tablet')) {
+                // User is on a mobile or tablet device
+                imageViewer.addEventListener('touchstart', (event) => {
+                    clearInterval(intervalId); // Stop den automatiske skift, når der trykkes
+                    event.preventDefault(); // Prevent default touch behavior
+                    changeImage();
+                    startAutoChange(); // Start den automatiske skift igen
+                });
+            } else {
+                // User is on a desktop computer
+                imageViewer.addEventListener('click', () => {
+                    clearInterval(intervalId); // Stop den automatiske skift, når der klikkes
+                    changeImage();
+                    startAutoChange(); // Start den automatiske skift igen
+                });
+            }
+        } else {
+            console.error('Ingen billeder blev fundet i mappen "images".');
+        }
+    })
+    .catch(error => {
+        console.error('Fejl ved indlæsning af billeder:', error);
+    });
